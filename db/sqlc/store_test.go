74a20e139546d8b460d37f4678d3a146018f77b8
Yusuf Ganiyu <airscholar@gmail.com>
2022-04-11 21:13:44 +0100

    Added unit testing for testing of account balance

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -34,6 +34,7 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// check the errors
+	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -74,6 +75,26 @@ func TestTransferTx(t *testing.T) {
 		_, err = testQueries.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
-		// TODO: check account balance
+		// check accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
+
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// check account balance
+		diff1 := account1.Balance - fromAccount.Balance
+		diff2 := account2.Balance - toAccount.Balance
+
+		require.Equal(t, diff1, diff2)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1 > 0)
+		require.True(t, diff1%amount == 0) // 1*amount, 2*amount, 3*amount ... n*amount == 0
+
+		k := int(diff1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
 	}
 }
